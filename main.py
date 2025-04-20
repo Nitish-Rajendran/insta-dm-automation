c4dac8b721e041dd481f28ed1f3c1ba4307090c7
Nitish <nitishrajendran.work@gmail.com>
2025-04-20 23:00:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from instabot import Bot
-import os
-import datetime
 
 bot = Bot()
-bot.login(username=os.getenv("IG_USERNAME"), password=os.getenv("IG_PASSWORD"))
-
-today = datetime.date.today()
-base = datetime.date(2025, 4, 20)
-count = (today - base).days + 1
-
-message = f"{count}. Good Morning"
-receiver = os.getenv("IG_RECEIVER")
-
-bot.send_message(message, [receiver])
+bot.login(username="_hsitin", password="hsitin_2006")
+bot.send_message("Morning BITCH!", ["_afiya._.28_"])
